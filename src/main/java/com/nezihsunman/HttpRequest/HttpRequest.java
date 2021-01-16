1e04dfc391906c788f6b5d9e0fcc4d2935351bd7
nezihsunman <36239952+nezihsunman@users.noreply.github.com>
2021-01-16 22:54:26 +0300

    aading rar

--- src/main/java/com/nezihsunman/HttpRequest/HttpRequest.java
+++ src/main/java/com/nezihsunman/HttpRequest/HttpRequest.java
@@ -7,13 +7,13 @@ import java.net.URL;
 import java.util.Base64;
 
 public class HttpRequest {
-    private static String basicAuth = "token " + "757b55e6c08ad07e5942711bdbdfcb758c96ef70 ";
+    private static String basicAuth = "token " + " ";
 
     public static InputStream sendHttpRequest(String url) throws IOException {
         URL obj = new URL(url);
         HttpURLConnection httpURLConnection = (HttpURLConnection) obj.openConnection();
         httpURLConnection.setRequestMethod("GET");
-        // httpURLConnection.setRequestProperty("Authorization", basicAuth);
+        //httpURLConnection.setRequestProperty("Authorization", basicAuth);
         httpURLConnection.setRequestProperty("accept", "application/vnd.github.v3+json");
         int responseCode = httpURLConnection.getResponseCode();
         if (responseCode == HttpURLConnection.HTTP_OK) { // success
